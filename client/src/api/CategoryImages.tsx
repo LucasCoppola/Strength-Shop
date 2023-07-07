a3d214ab88c82f570b49ec1f4538465026725a17
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-07-07 11:03:37 -0300

    removed href

--- client/src/api/CategoryImages.tsx
+++ client/src/api/CategoryImages.tsx
@@ -2,20 +2,17 @@ const imageCategory = [
 	{
 		name: 'Racks',
 		imageSrc: 'https://res.cloudinary.com/dotpfjpno/image/upload/v1688737823/AF-EVOX-STAND-L7_a542c71e-1c9e-416a-bb81-77e9414e6079_a0ehuv.webp',
-		imageAlt: 'Single gym rack',
-		href: '#'
+		imageAlt: 'Single gym rack'
 	},
 	{
 		name: 'Powerlifting',
 		imageSrc: 'https://res.cloudinary.com/dotpfjpno/image/upload/v1688737914/powerlifting_x8zwhg.jpg',
-		imageAlt: 'Bar with plates',
-		href: '#'
+		imageAlt: 'Bar with plates'
 	},
 	{
 		name: 'Calisthenics',
 		imageSrc: 'https://res.cloudinary.com/dotpfjpno/image/upload/v1688737804/001-Rogue-Fitness-Calisthenics-Parallettes-004-opt_prexfz.jpg',
-		imageAlt: 'Pair of parallettes',
-		href: '#'
+		imageAlt: 'Pair of parallettes'
 	}
 ]
 
